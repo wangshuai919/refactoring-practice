39e958617ff78eb02b5d8723bbd3a30de94862a8
Shuai Wang <shuai.wang1@thoughtworks.com>
2020-05-16 01:52:37 +0800

    add Owing

---
+++ src/main/java/practice/p01/Order.java
@@ -0,0 +1,8 @@
+package practice.p01;
+
+public class Order {
+    double amount;
+    public double getAmount() {
+        return 0;
+    }
+}
---
+++ src/main/java/practice/p01/Owing.java
@@ -0,0 +1,38 @@
+package practice.p01;
+
+import java.util.*;
+
+public class Owing {
+    private Vector orders;
+    private String name;
+
+    public Owing(Vector<Order> orders, String name) {
+        this.orders = orders;
+        this.name = name;
+    }
+
+    void printBill() {
+        double amount = 0.0;
+        Enumeration e = orders.elements();
+
+        printBanner();
+
+        System.out.println("***********Order**************");
+
+        //calculate amount
+        while (e.hasMoreElements()) {
+            Order each = (Order) e.nextElement();
+            amount += each.getAmount();
+        }
+
+        //print detail
+        System.out.println("name:" + name);
+        System.out.println("total:" + amount);
+    }
+
+    private void printBanner() {
+        System.out.println("**************************");
+        System.out.println("***** Customer Owes ******");
+        System.out.println("**************************");
+    }
+}
