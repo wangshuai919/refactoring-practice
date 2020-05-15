c7ab753fef9d09c4f62cb7974c58f466d242938d
Shuai Wang <shuai.wang1@thoughtworks.com>
2020-05-16 00:35:41 +0800

    add BaseTest.java

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,15 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class BaseTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
