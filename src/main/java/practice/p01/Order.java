bad022a2282a63988c4d070ef7d02622e0b4fbba
Shuai Wang <shuai.wang1@thoughtworks.com>
2020-05-16 01:54:57 +0800

    add Owing

--- src/main/java/practice/p01/Order.java
+++ src/main/java/practice/p01/Order.java
@@ -2,6 +2,11 @@ package practice.p01;
 
 public class Order {
     double amount;
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
     public double getAmount() {
         return 0;
     }
