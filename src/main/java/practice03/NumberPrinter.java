c1eefdee850666644bef4842500b67ea979d9cab
WangShuai <abc_billy@163.com>
2020-06-01 21:22:57 +0800

    add test case

--- src/main/java/practice03/NumberPrinter.java
+++ src/main/java/practice03/NumberPrinter.java
@@ -2,6 +2,15 @@ package practice03;
 
 public class NumberPrinter {
     public String print(int num) {
+        if (num % 5 == 0 && num % 3 == 0) {
+            return "FizzBuzz";
+        }
+        if (num % 5 == 0) {
+            return "Buzz";
+        }
+        if (num % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(num);
     }
 }
--- src/test/java/practice03/FizzBuzzTest.java
+++ src/test/java/practice03/FizzBuzzTest.java
@@ -5,14 +5,31 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class FizzBuzzTest {
+    private NumberPrinter printer = new NumberPrinter();
+
     @Test
     public void should_return_num_when_print() {
-        NumberPrinter printer = new NumberPrinter();
         assertTrue("1".equals(printer.print(1)));
     }
 
     @Test
     public void should_return_fizz_when_multiples_of_three() {
+        assertTrue("Fizz".equals(printer.print(3)));
+        assertTrue("Fizz".equals(printer.print(6)));
+        assertTrue("Fizz".equals(printer.print(9)));
+    }
 
+    @Test
+    public void should_return_buzz_when_multiples_of_five() {
+        assertTrue("Buzz".equals(printer.print(5)));
+        assertTrue("Buzz".equals(printer.print(10)));
+        assertTrue("Buzz".equals(printer.print(20)));
+    }
+
+    @Test
+    public void should_return_fizzbuzz_when_multiples_of_three_and_five() {
+        assertTrue("FizzBuzz".equals(printer.print(15)));
+        assertTrue("FizzBuzz".equals(printer.print(30)));
+        assertTrue("FizzBuzz".equals(printer.print(45)));
     }
 }
